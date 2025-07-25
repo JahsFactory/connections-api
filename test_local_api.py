f2bcc871257fb4619b47ad11c36e7a32916c8c08
jahcodes <davis.elijah.g@gmail.com>
2025-07-24 20:54:03 -0700

    simple py tests

--- test_local_api.py
+++ test_local_api.py
@@ -1,9 +1,9 @@
 import requests
 
-url = "https://connections-api-app.azurewebsites.net/api/daily"
+url = "http://localhost:5062/api/daily"
 
 try:
-    response = requests.get(url, timeout=10, verify=False)
+    response = requests.get(url)
     response.raise_for_status()
     print("Status Code:", response.status_code)
     print("Response JSON:")
